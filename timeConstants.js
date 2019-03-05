0204a755c881472ca6dbb89eba74853c6a440fa0
Wim <wim@widgets.nl>
2019-03-05 15:16:25 +0100

    Added interval constants to contain magic numbers

--- timeConstants.js
+++ timeConstants.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const SECOND = 1000;
-const MINUTE = 60 * SECOND;
-const HOUR = 60 * MINUTE;
-const DAY = 24 * HOUR;
-const WEEK = 7 * DAY;
+const MILLISECONDS_PER_SECOND = 1000;
+const SECONDS_PER_MINUTE = 60;
+const MINUTES_PER_HOUR = 60;
+const HOURS_PER_DAY = 24;
+const DAYS_PER_WEEK = 7;
+const MONTHS_PER_YEAR = 12;
+
+const SECOND = MILLISECONDS_PER_SECOND;
+const MINUTE = SECOND * SECONDS_PER_MINUTE;
+const HOUR = MINUTE * MINUTES_PER_HOUR;
+const DAY = HOUR * HOURS_PER_DAY;
+const WEEK = DAY * DAYS_PER_WEEK;
 const YEAR = DAY * 365.24;
 const NORMAL_YEAR = DAY * 365;
 const LEAP_YEAR = DAY * 366;
@@ -24,6 +31,12 @@ module.exports = {
     ,   DECADE : DECADE
     ,   HALF_YEAR: HALF_YEAR
     ,   AVERAGE_MONTH : AVERAGE_MONTH
+    ,   MILLISECONDS_PER_SECOND : MILLISECONDS_PER_SECOND
+    ,   SECONDS_PER_MINUTE : SECONDS_PER_MINUTE
+    ,   MINUTES_PER_HOUR : MINUTES_PER_HOUR
+    ,   HOURS_PER_DAY : HOURS_PER_DAY
+    ,   DAYS_PER_WEEK : DAYS_PER_WEEK
+    ,   MONTHS_PER_YEAR : MONTHS_PER_YEAR
     
     // ±100,000,000 days, the min and max dates allowed in ECMA Script.
     // See: http://ecma-international.org/ecma-262/5.1/#sec-15.9.1.1
